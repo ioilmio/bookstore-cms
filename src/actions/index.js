a5a626ba348a9f682e5a196fca3773353238ae0e
ioilmio <illuminato.salvatore@gmail.com>
2021-02-09 18:27:10 +0100

    fix eslint

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,4 @@
-
 import { CREATE_BOOK, REMOVE_BOOK, CHANGE_FILTER } from './actionsTypes';
-
 
 export const createBook = book => ({ type: CREATE_BOOK, book });
 export const removeBook = id => ({ type: REMOVE_BOOK, id });
